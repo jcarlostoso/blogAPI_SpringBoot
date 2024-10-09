75fcdd5a506f9bb359ffd30f49c6672cafbb5dc2
@bynoter <jcarlostoso@gmail.com>
2024-10-08 23:17:38 -0600

    Se crea interfaz ComentarioRepositorio

---
+++ src/main/java/com/jcarlostoso/blogApis/repositorio/ComentarioRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.jcarlostoso.blogApis.repositorio;
+
+import com.jcarlostoso.blogApis.entidades.Comentario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author bynot
+ */
+public interface ComentarioRepositorio extends JpaRepository<Comentario, Long>{
+    
+}
