f8939a0fb3819b717c33508c73cbed300bd89c3e
@bynoter <jcarlostoso@gmail.com>
2024-10-08 23:14:46 -0600

    Se crea relacion de los comentario en entidad Publicacion

--- src/main/java/com/jcarlostoso/blogApis/entidades/Publicacion.java
+++ src/main/java/com/jcarlostoso/blogApis/entidades/Publicacion.java
@@ -4,13 +4,17 @@
  */
 package com.jcarlostoso.blogApis.entidades;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Data;
 
 /**
@@ -33,6 +37,9 @@ private String descripcion;
 
 @Column(name = "contenido", nullable = false)
 private String contenido;
+
+@OneToMany(mappedBy = "publicacion", cascade = CascadeType.ALL,orphanRemoval = true)
+private Set<Comentario> comentarios = new HashSet<>();
 
     public Publicacion(Long id, String titulo, String descripcion, String contenido) {
         this.id = id;
